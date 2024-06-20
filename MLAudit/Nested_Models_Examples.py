44733670e4922c3f4bea296e065feb50639da648
rchynoweth <rchynoweth>
2024-06-20 23:00:51 +0000

    Adding nested models example

--- MLAudit/Nested_Models_Examples.py
+++ MLAudit/Nested_Models_Examples.py
@@ -2,7 +2,7 @@
 # MAGIC %md
 # MAGIC # Nested Models Example
 # MAGIC
-# MAGIC This notebook shows how best to train and register models that are nested in nature. For example, assume we have 1000 skus which we are generating forecasts, there should be 1 model registered with 1000 sub models. 
+# MAGIC This notebook shows how best to train and register models that are nested in nature. For example, assume we have 10 skus which we are generating forecasts, there should be 1 model registered with 10 sub models. 
 # MAGIC
 # MAGIC This notebook was created using DBR 14.3LTS Machine Learning. For more information, please refer to this [MLflow Documentation](https://mlflow.org/docs/latest/traditional-ml/hyperparameter-tuning-with-child-runs/part1-child-runs.html) on parent/child runs. 
 
@@ -14,6 +14,9 @@ import pandas as pd
 import numpy as np
 from pyspark.sql.functions import *
 from pyspark.sql.types import * 
+
+mlflow.set_registry_uri("databricks-uc")
+
 
 # COMMAND ----------
 
@@ -66,6 +69,7 @@ display(spark_df)
 
 # COMMAND ----------
 
+# expected schema output
 forecast_result_schema = StructType([
       StructField('ds',DateType()),
       # StructField('workspace_id', StringType()),
@@ -79,13 +83,17 @@ forecast_result_schema = StructType([
 
 # COMMAND ----------
 
+# wrapping the generate_forecasts allows us to nest the child
+# models into a single experiment run for production deployment
+# We need to pass run_id to the function to keep the 
+# parent/child relationship 
+
 def create_forecast(run_id):
   def generate_forecast(self, history_pd):
     sku = history_pd['sku'].iloc[0]
 
-    with mlflow.start_run(nested=True, experiment_id=exp_id, tags={'sku': sku}, run_id=run_id):
+    with mlflow.start_run(nested=True, experiment_id=exp_id, run_id=run_id):
       history_pd = history_pd.dropna()
-      print(f"------- SKU: {sku}")
       
       # train and configure the model
       model = Prophet( interval_width=0.85 )
@@ -97,6 +105,9 @@ def create_forecast(run_id):
         freq='d', 
         include_history=True
         )
+      
+      # log model to workspace experiment with unique name
+      # the unique name allows us to look up the appropriate model later on 
       mlflow.prophet.log_model(model, artifact_path=f"prophet_model_{sku}")
       forecast_pd = model.predict( future_pd )  
       
@@ -121,13 +132,10 @@ def create_forecast(run_id):
 # COMMAND ----------
 
 
-
-# COMMAND ----------
-
-
+# Execute the training run 
 with mlflow.start_run(experiment_id=exp_id) as run:
       run_id = run.info.run_id
-
+      # group by sku and train a model for each sku
       (spark_df
        .groupBy('sku')
        .applyInPandas(create_forecast(run_id), schema=forecast_result_schema)
@@ -135,6 +143,99 @@ with mlflow.start_run(experiment_id=exp_id) as run:
        .mode('overwrite')
        .saveAsTable('rac_demo_catalog.default.output_prophet_test')
       )
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Examine the ML flow Experiment
+# MAGIC
+# MAGIC Navigating to the experiment you will see that each of the 10 models are logged to the same experiment run.  
+# MAGIC
+# MAGIC <img src="https://github.com/rchynoweth/Demo/blob/cf51bf92d8ed07df7cce73956022416326ff4778/MLAudit/ModelList.png?raw=true"/> 
+# MAGIC
+# MAGIC
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Use Models in Batch Inference
+
+# COMMAND ----------
+
+# get some test data
+test_df = df_6_months[df_6_months['sku'] == 'SKU_1']
+test_df.head()
+
+# COMMAND ----------
+
+# get the latest run we executed previously
+latest_exp_run = mlflow.search_runs(experiment_ids=[exp_id]).sort_values('start_time', ascending=False).iloc[0]
+run = mlflow.get_run(latest_exp_run.run_id)
+
+# COMMAND ----------
+
+# Python wrapper class 
+import mlflow 
+
+class BatchModel(mlflow.pyfunc.PythonModel):
+  def __init__(self, run_id):
+    self.client = mlflow.MlflowClient()
+    self.run = mlflow.get_run(run_id)
+    self.artifact_path = self.run.info.artifact_uri
+    self.models = {}
+    self.load_models()
+
+  ## NOTICE - 
+  # If you have a LOT of models 
+  # it may be too big to fit into memory and you may want to 
+  # parse it out into smaller chunks 
+  def load_models(self):
+    # get all the models
+    self.artifacts = self.client.list_artifacts(run.info.run_id)
+    # for all models in the run, we want to load them into a dict
+    for a in self.artifacts:
+      self.models[a.path] = mlflow.prophet.load_model(f"{self.artifact_path}/{a.path}")
+
+  def predict(self, model_input):
+    # get the incoming data sku
+    sku = model_input['sku'].iloc[0]
+    # format the model name for look up
+    model_name = f"prophet_model_{sku}"
+    # load the model
+    model = self.models.get(model_name)
+    # forecast the data
+    forecast_pd = model.predict( model_input )
+    # return the output
+    return forecast_pd
+
+
+# COMMAND ----------
+
+# create wrapper model object
+model = BatchModel(run.info.run_id)
+
+# COMMAND ----------
+
+# make prediction
+output = model.predict(test_df)
+display(output)
+
+# COMMAND ----------
+
+# log wrapper model
+model_name = "custom_model"
+with mlflow.start_run() as register_run:
+  mlflow.pyfunc.log_model(
+    artifact_path=model_name, 
+    python_model=model,
+    signature=mlflow.models.infer_signature(test_df, output)
+    )
+
+# COMMAND ----------
+
+model_uri = f'runs:/{register_run.info.run_id}/{model_name}'
+reg = mlflow.register_model(model_uri=model_uri, name="rac_demo_catalog.default.custom_prophet_model", await_registration_for=600) 
+
 
 # COMMAND ----------
 
